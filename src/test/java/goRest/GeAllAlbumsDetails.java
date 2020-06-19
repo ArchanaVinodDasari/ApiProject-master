d1bc9612e6fcf9d1bb28cb5fbae3fba05b4a1e65
ArchanaN <archana.testprojects@gmail.com>
2020-06-19 22:31:50 +0530

    goRest.java changes

---
+++ src/test/java/goRest/GeAllAlbumsDetails.java
@@ -0,0 +1,35 @@
+package goRest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.RestAssured;
+
+public class GeAllAlbumsDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Get Method to get all the album details");
+		
+		RestAssured.baseURI="https://gorest.co.in/public-api/albums";
+		RestAssured.useRelaxedHTTPSValidation();
+		given()
+			.queryParam("access-token","fqOKuj5gT0OS4DErEXl0VB1lbPXQxN3IZm8O")
+			//.auth().basic("ArchanaN","password")
+			.body("")
+		.when()
+			.get()
+		.then().log().all()
+			.assertThat().statusCode(200)
+			.header("Server", equalTo("nginx"))
+			.body("result.id[0]", equalTo("8"))
+		;
+		
+		System.out.println("Get Method - Single User with Bearer Authentication - PASS");
+	}
+
+
+
+	}
+
+
---
+++ src/test/java/goRest/GetAllAlbumsSingleUser.java
@@ -0,0 +1,10 @@
+package goRest;
+
+public class GetAllAlbumsSingleUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
